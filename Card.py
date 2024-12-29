5f4ceb471faa1d064d6bfc27ade56e455e74c1c7
Victor Ferreira Silva <mail@victorf.ch>
2024-12-29 20:06:52 +0000

    first commit

---
+++ Card.py
@@ -0,0 +1,4 @@
+class Card(object):
+    def __init__(self, c, v):
+        self.color = c
+        self.value = v
---
+++ Game.py
@@ -0,0 +1,8 @@
+# add Card class from Card.py (this folder) and instantiate new card 
+
+from Card import Card
+
+card = Card("R", 5)
+
+print(card.color)
+print(card.value)
